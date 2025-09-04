7f5652fb52b647abe78892c86450612d944a13fb
AnneRom <coolann000.com@gmail.com>
2025-09-04 15:12:49 +0300

    Створення проєкту

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,9 @@
+function TaskItem() {
+  return (
+    <header>
+      <h1>Список завдань</h1>
+    </header>
+  );
+}
+
+export default TaskItem;
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,9 @@
+function TaskList() {
+  return (
+    <header>
+      <h1>Список завдань</h1>
+    </header>
+  );
+}
+
+export default TaskList;
