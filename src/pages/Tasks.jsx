7b1c12f7be82c6acb81f500d2a93103942138f06
AnneRom <coolann000.com@gmail.com>
2025-12-16 18:40:18 +0200

    16.12
    Додали два фільтра до списку завдань та вибір сортування, поміняли структуру рендеру списку: стан -> фільтр - > сортування -> рендер.

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -3,38 +3,26 @@ import TaskList from '../components/tasks/TaskList'
 import TaskForm from '../components/tasks/TaskForm'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-
-const order = {
-    High: 1,
-    Medium: 2,
-    Low: 3
-  };
-  
-  const sortTasks = (list) => {
-  return [...list].sort((a, b) => {
-    if (a.completed !== b.completed) {
-      return a.completed ? 1 : -1;
-    }
-    
-
-    if (a.deadline && b.deadline) {
-      return new Date(a.deadline) - new Date(b.deadline);
-    } else if (a.deadline !== b.deadline) {
-      return a.deadline ? -1 : 1;
-    }
-
-    return order[a.priority] - order[b.priority];
-  });
-};
+import styles from "../styles/Tasks.module.scss";
+import { useSearchParams } from "react-router-dom";
 
 const Tasks = () => {
     const [tasks, setTasks] = useState([
     // { id: 1, text: "Learn React", priority: "High", completed: false },
   ]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const status = searchParams.get("status") || "all";
+    const priority = searchParams.get("priority") || "all";
+    const sort = searchParams.get("sort") || "deadline";
+    const order = {
+        High: 1,
+        Medium: 2,
+        Low: 3
+    };
 
-  useEffect(() => {
-    setTasks(prev => sortTasks(prev));
-  }, []);
+  // useEffect(() => {
+  //   setTasks(prev => sortTasks(prev));
+  // }, []);
 
 
   useEffect(() => {
@@ -45,8 +33,8 @@ const Tasks = () => {
       try {
         const parsedTasks = JSON.parse(savedTasks);
         console.log("Parsed tasks:", JSON.parse(savedTasks));
-        // setTasks(parsedTasks); 
-        setTasks(sortTasks(parsedTasks));
+        setTasks(parsedTasks); 
+        // setTasks(sortTasks(parsedTasks));//тут не сортувати
         return
       } catch (e) {
         console.error("Failed to parse tasks from localStorage:", e);
@@ -58,8 +46,8 @@ const Tasks = () => {
         const response = await axios.get(
         "https://my-json-server.typicode.com/AnneRom/TO-DO-LIST-API/tasks"
         );
-        // setTasks(response.data);
-        setTasks(sortTasks(response.data));
+        setTasks(response.data);
+        // setTasks(sortTasks(response.data));
       } catch (e) {
         console.error("Error fetching tasks:", e);
       }
@@ -91,31 +79,129 @@ const Tasks = () => {
      };
     //  setTasks([...tasks, newTask]);
     // setTasks(sortTasks([...tasks, newTask]));
-    setTasks(prevTasks => sortTasks([...prevTasks, newTask]));
+    setTasks(prevTasks => [...prevTasks, newTask]);
+    // setTasks(prevTasks => sortTasks([...prevTasks, newTask]));
   }
 
   const deleteTask = (id) => {
     // setTasks(tasks.filter(task => task.id !== id));
-    setTasks(prev => sortTasks(prev.filter(task => task.id !== id)));
+    // setTasks(prev => sortTasks(prev.filter(task => task.id !== id)));
+    setTasks(prev => prev.filter(task => task.id !== id));
   }
   const toggleTask = (id) => {
     // setTasks(tasks.map(task => 
     //   task.id === id ? { ...task, completed: !task.completed } : task
     // ));
-    setTasks(prev => sortTasks(
-    prev.map(task =>
-      task.id === id ? { ...task, completed: !task.completed } : task
-    )
-  ));
+  //   setTasks(prev => sortTasks(
+  //   prev.map(task =>
+  //     task.id === id ? { ...task, completed: !task.completed } : task
+  //   )
+  // ));
+    setTasks(prev =>
+      prev.map(task =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
   }
+  const filtered = tasks
+  .filter(item => {
+    if (status === "completed") return item.completed;
+    if (status === "incomplete") return !item.completed;
+    return true;
+  })
+  .filter(item => {
+    if (priority === "high") return item.priority === "High";
+    if (priority === "medium") return item.priority === "Medium";
+    if (priority === "low") return item.priority === "Low";
+    return true;
+  })
+
+  const sortTasks = [...filtered].sort((a, b) => {
+        if (sort === "priority") {
+          return order[a.priority] - order[b.priority];
+        }
+        if (sort === "added") {
+          return a.id - b.id;
+        }
+
+        if (a.completed !== b.completed) {
+        return a.completed ? 1 : -1;
+        }
+
+        if (a.deadline && b.deadline) {
+        return new Date(a.deadline) - new Date(b.deadline);
+        } else if (a.deadline !== b.deadline) {
+        return a.deadline ? -1 : 1;
+        }
+
+        
+    });
+  
+  const updateFilters = (key, value) => {
+     // 1. Копіюємо існуючі параметри
+    const updatedParams = new URLSearchParams(searchParams); 
+    
+    // 2. Оновлюємо певний ключ
+    updatedParams.set(key, value); 
+    
+    // 3. Застосовуємо зміни до URL
+    setSearchParams(updatedParams); 
+  }
+  const [priorityFilter, setPriorityFilter] = useState([]);
 
     return (
         <div className="mainContainer">
+            <div className={styles.filters}>
+                <select 
+                value={status} 
+                onChange={(e) => updateFilters('status', e.target.value)}>
+                    <option value="all">Всі</option>
+                    <option value="completed">Виконані</option>
+                    <option value="incomplete">Невиконані</option>
+                </select>
+                <label><input type="checkbox"
+                value={priority}
+                checked={priorityFilter.includes('High')}
+                onChange={(e) => updateFilters('priority', e.target.value)}
+                 /> High</label>
+
+                <label><input type="checkbox"
+                value={priority}
+                checked={priorityFilter.includes('Medium')}
+                onChange={(e) => updateFilters('priority', e.target.value)} /> Medium</label>
+
+                <label><input type="checkbox"
+                value={priority}
+                checked={priorityFilter.includes('Low')}
+                onChange={(e) => updateFilters('priority', e.target.value)} /> Low</label>
+                
+                <select 
+                value={priority} 
+                onChange={(e) => updateFilters('priority', e.target.value)}>
+                    <option value="all">All</option>
+                    <option value="high">High</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Low</option>
+                </select>
+
+                 <select 
+                value={sort} 
+                onChange={(e) => updateFilters('sort', e.target.value)}>
+                    <option value="deadline">за терміном</option>
+                    <option value="added">за датою додавання</option>
+                    <option value="priority">за пріоритетом</option>
+                </select>
+                Додали два фільтра до списку завдань та вибір сортування, поміняли структуру рендеру списку: стан -> фільтр - > сортування -> рендер.
+
+
+
+                
+            </div>
             <TaskForm onAdd={addTask}/>
             <TaskTitle />
-            <TaskList tasks={tasks} onDelete={deleteTask} onToggle={toggleTask}/>
+            <TaskList tasks={sortTasks} onDelete={deleteTask} onToggle={toggleTask}/>
        </div>
     );
 
-}
+};
 export default Tasks;
